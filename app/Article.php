ba536cbd997a8f2ec51bf7e839a2b434135133d9
trans-leasing <emailoflvs@gmail.com>
2022-04-20 23:18:20 +0200

    make getLibraryArticlesToUsers

--- app/Article.php
+++ app/Article.php
@@ -32,17 +32,46 @@ class Article extends Model
     }
 
     /**
-     * Get Article by id
+     * Get Article name by id
      *
      * @var string
      */
-    public static function getArticleById(int $article_id)
+    public static function getArticleNameById(int $article_id)
     {
 
         $article = Article::find($article_id);
         return is_object($article) ? $article->text : "";
 
     }
+
+
+    /**
+     * Receive scheme where article show all readers
+     *
+     * @var array
+     */
+    public static function getLibraryArticlesToUsers()
+    {
+
+        $articleToUsers = [];
+        foreach (Article::all() as $article) {
+
+            $lib['id'] = $article['id'];
+            $lib['text'] = Article::getArticleNameById($article['id']);
+
+            $readers = [];
+            $reader = Article::find($article['id']);
+            foreach ($reader->users as $user) {
+                $readers[] = $user['name'];
+            }
+            $lib['name'][] = $readers;
+
+            $articleToUsers[] = $lib;
+        }
+
+        return $articleToUsers;
+    }
+
 
 
 }
