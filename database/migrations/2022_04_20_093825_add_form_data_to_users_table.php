f9e65f02453dac44d7bc296d9ba4876390651aa7
trans-leasing <emailoflvs@gmail.com>
2022-04-20 18:55:54 +0200

    add nullable for articles

--- database/migrations/2022_04_20_093825_add_form_data_to_users_table.php
+++ database/migrations/2022_04_20_093825_add_form_data_to_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddVotesToUsersTable extends Migration
+class AddFormDataToUsersTable extends Migration
 {
     /**
      * Run the migrations.
